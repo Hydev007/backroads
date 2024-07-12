f0bafb9a76b468a80f959f253d71da3a2c928038
John <evilhydev007@gmail.com>
2024-07-12 12:51:50 -0300

    changes Navbar - add data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Footer from './components/Footer';
 
 function App() {
   return (
-   <h1>Backroads app</h1>
+   <>
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Tours />
+    <Footer />
+   </>
   );
 }
 
