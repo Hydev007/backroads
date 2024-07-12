cf67c285cf162aadf0f264350f7577dbbed62f2a
John <evilhydev007@gmail.com>
2024-07-12 13:43:57 -0300

    changes Title - PageLink - Hero

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
-const Title = () => {
-  return (
-    <div>
-      Title
-    </div>
-  )
-}
-
-export default Title
+const Title = ({ title, subTitle }) => {
+    return (
+      <div className='section-title'>
+        <h2>
+          {title} <span>{subTitle}</span>
+        </h2>
+      </div>
+    );
+  };
+  export default Title;
+  
